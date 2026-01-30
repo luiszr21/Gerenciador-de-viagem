1b9a11b3a1f28340b11266f3970f85764ec8f0d1
luis matheus <matheusdsf94@gmail.com>
2026-01-30 02:16:51 -0300

    feito o front da tela de login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import "./login.css";
+import background from "../images/background.jpg";
 export function Login() {
   const { authenticate, loading, error } = useAuth();
 
@@ -12,36 +13,47 @@ export function Login() {
     authenticate(email, senha);
   }
 
-  return (
-    <form onSubmit={handleSubmit}>
 
-      <div className="container-login">
-        <h1 className = "titulo" >Login</h1>
+return (
+  <section
+    className="login-section"
+    style={{ backgroundImage: `url(${background})` }}
+  >
+    <div className="overlay"></div>
+    <div>
+      <div className="welcome-message">
+        <h1 className = "titulo_login" >Tackoff</h1>
+        <p className = "paragrafo_login">Seu próximo destino começa aqui</p>
+      </div>
+      
+      <form onSubmit={handleSubmit} className="container-login">
+        <h1 className="titulo">Inicie Sua Jornada</h1>
         <input
-        className="input-login"
+          className="input-login"
           value={email}
           onChange={e => setEmail(e.target.value)}
           placeholder="Email"
         />
         <input
-        className="input-login"
+          className="input-login"
           type="password"
           value={senha}
           onChange={e => setSenha(e.target.value)}
           placeholder="Senha"
         />
-        <button className='botao' type="submit" disabled={loading}>
+        <p className="esqueceu-senha">
+          Esqueceu sua senha? <a href="#">Clique aqui</a>
+        </p>
+        <button className="botao" type="submit" disabled={loading}>
           {loading ? "Entrando..." : "Entrar"}
         </button>
-      </div>
-
-    <div>
-      <p>
-      </p>
-      <p> 
-      </p>
+        <p className="cadastre-se">
+          Não tem uma conta? <a href="#">Cadastre-se</a>
+        </p>
+        {error && <p className="erro">{error}</p>}
+      </form>
     </div>
-      {error && <p>{error}</p>}
-    </form>
-  );
+  </section>
+);
+
 }
